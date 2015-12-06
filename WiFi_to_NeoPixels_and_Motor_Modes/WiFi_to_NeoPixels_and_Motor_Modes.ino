f4dde775d542bceaf0fdd9fcf00a005510ef42cf
Byrne <RichardByr@youforce.net>
2015-12-06 21:22:48 +0100

    Added a gamma map - not sure I like it yet - The end is too bright

--- WiFi_to_NeoPixels_and_Motor_Modes/WiFi_to_NeoPixels_and_Motor_Modes.ino
+++ WiFi_to_NeoPixels_and_Motor_Modes/WiFi_to_NeoPixels_and_Motor_Modes.ino
@@ -14,18 +14,37 @@ bool getNewData=false; //A flag that stops animated display modes in order to ge
 bool allowModeChange;  //A flag to show that we should allow a mode change
 int modeNumberToUse;  //The last display mode received
  
-int ledPin_12 = 12;  // Vibrating Motor connected to digital pin 12
-int ledPin_13 = 13;  // Vibrating Motor connected to digital pin 13     
+//***************Gamma correction table for LED brightness************************//
+static const uint8_t gammaCorrect[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+    
 
 //***************Set Outputs************************//
-
-
 NeoPixelBus strip_04 = NeoPixelBus(pixelCount, 04);  //GPIO 4 Note 4 and 5 have their markings swapped on the vertical break out board
 NeoPixelBus strip_05 = NeoPixelBus(pixelCount, 05); 
 NeoPixelBus strip_15 = NeoPixelBus(pixelCount, 15); 
 NeoPixelBus strip_02 = NeoPixelBus(pixelCount, 02);
 NeoPixelBus strip_14 = NeoPixelBus(pixelCount, 14);
 
+int ledPin_12 = 12;  // Vibrating Motor connected to digital pin 12
+int ledPin_13 = 13;  // Vibrating Motor connected to digital pin 13 
+
+//***************Set WiFi connecion************************//
 //const char* ssid     = "Hippy-Laptop";
 //const char* password = "12345678";
 
@@ -165,7 +184,7 @@ void loop() {
  * WiFi
  */
 void handle_command() {
-  Serial.println("handle_command: ");
+  Serial.println("Command Received: ");
   server.send(200, "text/html", webResponse);
     if (server.hasArg("mode")) {
       String modeToUse = server.arg("mode");  //get mode number
@@ -191,8 +210,8 @@ void handle_command() {
 void DebugLED(int red, int green, int blue) {
 
   Serial.println("In DebugLED");
-   
-  RgbColor rgbColorToUse = RgbColor(red, green, blue); 
+
+  RgbColor rgbColorToUse = RgbColor(gammaCorrect[red], gammaCorrect[green], gammaCorrect[blue]); 
 
   for(int i=0; i < pixelCount; i++) 
   {
@@ -220,8 +239,10 @@ void Set_Mode_01() {
   rgbStr.replace("%23","#"); //%23 = # in URI
   int rgb[3];                           
   getRGB(rgbStr,rgb);
+
+  RgbColor rgbColorToUse = RgbColor(gammaCorrect[rgb[0]], gammaCorrect[rgb[1]], gammaCorrect[rgb[2]]); 
     
-  RgbColor rgbColorToUse = RgbColor(rgb[0], rgb[1], rgb[2]); 
+  //RgbColor rgbColorToUse = RgbColor(rgb[0], rgb[1], rgb[2]); 
  
   for(int i=0; i < pixelCount; i++) 
   {
@@ -799,7 +820,6 @@ void Set_Mode_11()
 /*
  * Utilities for display
  */
-
 
 int convertToInt(char upper,char lower)
 {
